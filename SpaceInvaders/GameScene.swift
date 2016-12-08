ab2b303b6011e60bbf47671e118de4c2be6a1d96
Will Hope <student@macbook-pro-hope-4.local>
2016-12-08 15:48:28 -0500

    I have added my character into the game.

--- SpaceInvaders/GameScene.swift
+++ SpaceInvaders/GameScene.swift
@@ -11,18 +11,34 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    
+    // Lets add your character
+    
+    let character = SKSpriteNode(imageNamed: "Character")
+    
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
         
+        // Let's add where your Character is
+        character.position = CGPoint(x: 200, y: 100)
+        addChild(character)
+        character.setScale(1.5)
+        
+        // Adding the enemies for the first time
         for enemiesY in 1...4 {
             for enemiesX in 1...6 {
                 let enemy = SKSpriteNode(imageNamed: "Enemies")
                 enemy.anchorPoint = CGPoint(x: 0, y: 0)
-                enemy.position = CGPoint(x: 50 * enemiesX, y: 350 + (enemiesY * 50))
-                enemy.setScale(0.08)
+                enemy.position = CGPoint(x: 50 * enemiesX, y: 475 + (enemiesY * 50))
+                enemy.setScale(0.05)
                 addChild(enemy)
+                
+
             }
         }
     }
     
 }
+
+
+
