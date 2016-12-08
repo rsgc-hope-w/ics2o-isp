4024b98b19323d6fafffb36951098283bbbb5e11
Will Hope <student@macbook-pro-hope-4.local>
2016-12-08 16:30:24 -0500

    I have gotten my character to move. Now just need to make it move smoothly from point to point.

--- SpaceInvaders/GameScene.swift
+++ SpaceInvaders/GameScene.swift
@@ -20,7 +20,7 @@ class GameScene: SKScene {
         backgroundColor = SKColor.black
         
         // Let's add where your Character is
-        character.position = CGPoint(x: 200, y: 100)
+        character.position = CGPoint(x: size.width / 2, y: 100)
         addChild(character)
         character.setScale(1.5)
         
@@ -37,8 +37,32 @@ class GameScene: SKScene {
             }
         }
     }
+    // This method lets us move our character with a single touch
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Our fingers are big when we touch a screen
+        guard let touch = touches.first else {
+            return
+        }
+        // Get the location of the first touch
+        let touchLocation = touch.location(in: self)
+        
+        // Printing the location of the touch out in the console
+        print(touchLocation.x)
+        print(touchLocation.y)
+        
+        // Making your character move horizontally toward your touch
+        let destination = CGPoint(x: touchLocation.x, y: character.position.y)
+        
+        // Create an action
+        let actionMove = SKAction.move(to: destination, duration: 1)
+        
+        // Tell the Character sprite to move
+        character.run(actionMove)
+        
     
 }
 
 
 
+}
